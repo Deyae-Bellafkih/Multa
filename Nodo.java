7aa9a0287c479db997b4abea1e2d3d24cb8f091d
Deyae-Bellafkih <bellafkih.deyae.studente@itispaleocapa.it>
2026-03-05 08:24:59 +0100

    Add files via upload

---
+++ Nodo.java
@@ -0,0 +1,12 @@
+package com.example.primaprova;
+
+public class Nodo {
+
+    public String value;
+    public Nodo next;
+
+    public Nodo(String value) {
+        this.value = value;
+        this.next = null;
+    }
+}
